64ab46f9bc207b2a3fa9768a44332a7b65fa17fe
mu88 <4560672+mu88@users.noreply.github.com>
2022-05-13 11:41:30 +0200

    Update marker's popup

--- src/WebApp/Shared/LifePointDetail.razor.js
+++ src/WebApp/Shared/LifePointDetail.razor.js
@@ -1,9 +1,27 @@
-﻿export function createMarker(leafletMap, id, latitude, longitude, dotNetMapReference) {
+﻿let _markers = [];
+let _dotNetMapReference;
+
+export function createMarker(leafletMap, id, latitude, longitude, dotNetMapReference) {
+    if (!_dotNetMapReference) {
+        _dotNetMapReference = dotNetMapReference;
+    }
+    
     dotNetMapReference.invokeMethodAsync('LogAsync', "Start createMarker");
     let marker = L.marker([latitude, longitude]);
+    marker._id = id;
     dotNetMapReference.invokeMethodAsync('LogAsync', "Marker created");
     marker.bindPopup("<life-point-detail id='" + id + "'></life-point-detail>");
     dotNetMapReference.invokeMethodAsync('LogAsync', "Popup bound");
     marker.addTo(leafletMap);
     dotNetMapReference.invokeMethodAsync('LogAsync', "Marker added to map");
+    _markers.push(marker);
+}
+
+export function updatePopup(id) {
+    _markers.forEach(function (marker) {
+        if (marker._id === id) {
+            marker.getPopup().update();
+            _dotNetMapReference.invokeMethodAsync('LogAsync', "Popup of marker updated");
+        }
+    })
 }
